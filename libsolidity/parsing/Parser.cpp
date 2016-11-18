265faf72dcecced4d870c059cf1598d3327f7ec9
Alex Beregszaszi <alex@rtfs.hu>
2016-11-18 00:41:35 +0000

    Support view/pure FunctionType

--- libsolidity/parsing/Parser.cpp
+++ libsolidity/parsing/Parser.cpp
@@ -737,7 +737,8 @@ ASTPointer<FunctionTypeName> Parser::parseFunctionType()
 		header.parameters,
 		header.returnParameters,
 		header.visibility,
-		header.isDeclaredConst,
+		header.isView,
+		header.isPure,
 		header.isPayable
 	);
 }
